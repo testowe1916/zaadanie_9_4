fb321bfbf8e18b48e3eb3a9a4b3ce7184b9eb802
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-12-18 23:17:17 +0100

    Mod js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function drawTree(x) {
-	for(var i=0; i<=x; i++) {
+	for(var i=1; i<=x; i++) {
 		var star='*';
-		for(var j=0; j<i; j++) {
+		for(var j=1; j<i; j++) {
 			star+='*';
 		};
 		console.log(star);
