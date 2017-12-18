e5bc21c44e515b07ffd2f7c65c37dc6264f45556
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-12-18 20:19:30 +0100

    Mod js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 function drawTree(x) {
 	for(var i=0; i<=x; i++) {
-		var star='';
+		var star='*';
 		for(var j=0; j<i; j++) {
 			star+='*';
 		};
 		console.log(star);
-		alert(star);
 	};
 };
 drawTree(5);
